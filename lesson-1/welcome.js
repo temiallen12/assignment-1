1f8b36e18df2a87cadd98ebe2f8a50cd570c4f55
temiallen <zephtemi@gmail.com>
2023-07-07 12:21:08 +0100

    answer01

--- lesson-1/welcome.js
+++ lesson-1/welcome.js
@@ -3,7 +3,3 @@ let Second_Name = "Smith";
 let Age = 20;
 console.log("welcome Mr " + "" +First_name + " " + "your years is " + " " + Age)
 // QUestion two new variable 
-let num_one = prompt (394852);
-let num_two = prompt (242483);
-let sum_num = num_one + num_two;
-console.log(sum_num)
---
+++ lesson-2/answer01.js
@@ -0,0 +1,24 @@
+// let prompt = require('prompt-sync')({sigint: true})
+// number one answer 
+let First_Name = "lanre"
+let Last_Name = "atimo"
+let Age = 56;
+let Current_Year = 2003;
+let Earthly_Apperance = Age-Current_Year;
+console.log( First_Name,Last_Name + " " + "You are" + " " + Age + " " + "Years old and you came to the Earth in the Year" + "" + Earthly_Apperance)
+// number one answer end 
+
+// number two student queation
+let Name = "teslim"
+let age = 34;
+let Department_Name ="Food and nutrition "
+let Score = 34.72;
+console.log (`Welcome ${Name}, You are in ${Department_Name}, You are ${age}years old and your score is ${Score}`)
+// number two student queation
+
+// let pounds = Number(prompt('Enter weight(pounds): '))
+// let kg = pounds * 0.45;
+
+// console.log( `Your weight in kilograms is ${kg}`)
+
+
